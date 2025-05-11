8a8a7e7cb8c338d3e55400d5d029ce7c08a02694
Fa-Syaka-diouf <dfasyaka@ept.sn>
2025-05-11 19:24:15 +0000

    Premier commit

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from bson import ObjectId
 from mongo import db  
 
 def get_events(request):
@@ -7,3 +8,23 @@ def get_events(request):
         return render(request, 'events.html', {'events': events})
     else:
         return render(request, 'events.html', {'events': []})
+def get_historique(request, pk):
+    if db is not None:
+        try:
+            user_id = ObjectId(pk)
+        except Exception as e:
+            return render(request, 'historique.html', {'error': f"ID invalide: {e}"})
+
+        user = db.users.find_one({"_id": user_id})
+        if not user:
+            return render(request, 'historique.html', {'error': "Utilisateur non trouvé."})
+
+       
+        events = list(db.events.find({"creator": user_id}))
+
+        return render(request, 'historique.html', {
+            'events': events,
+            'user': user
+        })
+    else:
+        return render(request, 'historique.html', {'error': "Base de données non disponible."})
