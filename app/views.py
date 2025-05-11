a59ae60c5f249e00c5db6b607354d83b35abee4c
urlovefatima <fifidiey@icloud.com>
2025-05-11 15:10:03 +0000

    ajustement

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from mongo import db  
 
 def get_events(request):
-    if db:
+    if db is not None :
         events = list(db.events.find({}, {"_id": 0}))
         return render(request, 'events.html', {'events': events})
     else:
