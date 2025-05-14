570788f05ff2f7762780397f7c7a5635f0deb068
Fa-Syaka-diouf <dfasyaka@ept.sn>
2025-05-14 12:39:44 +0000

    Premier commit

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from bson import ObjectId
 from mongo import db  
 
@@ -8,23 +8,25 @@ def get_events(request):
         return render(request, 'events.html', {'events': events})
     else:
         return render(request, 'events.html', {'events': []})
-def get_historique(request, pk):
-    if db is not None:
-        try:
-            user_id = ObjectId(pk)
-        except Exception as e:
-            return render(request, 'historique.html', {'error': f"ID invalide: {e}"})
 
-        user = db.users.find_one({"_id": user_id})
-        if not user:
-            return render(request, 'historique.html', {'error': "Utilisateur non trouvé."})
+def delete_event(request):
+    if request.method == "POST":
+        event_id = request.POST.get("event_id")
+        if event_id:
+            db.events.delete_one({"_id": ObjectId(event_id)})
+        return redirect("delete_event")
 
-       
-        events = list(db.events.find({"creator": user_id}))
+    events = list(db.events.find())
+    for e in events:
+        e["id"] = str(e["_id"])
+    return render(request, "delete_event.html", {"events": events})
 
-        return render(request, 'historique.html', {
-            'events': events,
-            'user': user
-        })
-    else:
-        return render(request, 'historique.html', {'error': "Base de données non disponible."})
+def create_event(request):
+    if request.method== "POST":
+        titre = request.POST.get("titre")
+        categorie = request.POST.get("categorie")
+        localisation = request.POST.get("localisation")
+        date_heure = request.POST.get("date_heure")
+        capacite = request.POST.get("capacite")
+        description = request.POST.get("description")
+    
